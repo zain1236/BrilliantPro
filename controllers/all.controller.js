f215931b4a1c579ab3c5a69e4507ca2d6cd530e2
zain <saimzain1236@gmail.com>
2023-05-12 03:10:01 +0500

    ok

--- controllers/all.controller.js
+++ controllers/all.controller.js
@@ -6,7 +6,7 @@ exports.summary = async (req, res) => {
     try {
 
         // const bookValidate = await bookSchema.validateAsync(req.body);
-        const course = new model.Material.countDocuments();
+        const course = model.Material.countDocuments();
         console.log(course);
         res.status(200).send({"Message" : "success","data" : result})            
     }
