5247ef9cdf2a2a8a3488912fc1fd208350ff4a57
zain <saimzain1236@gmail.com>
2023-05-12 03:18:22 +0500

    Ok

--- controllers/all.controller.js
+++ controllers/all.controller.js
@@ -6,10 +6,19 @@ exports.summary = async (req, res) => {
     try {
 
         // const bookValidate = await bookSchema.validateAsync(req.body);
-        const course = (await model.Material.find()).length;
+        const material = (await model.Material.find()).length;
+        const course = (await model.Course.find()).length;
+        const learner = (await model.User.find({'role' : 'learner'})).length;
 
+
+        const data = {
+            "courses" : course,
+            "learners" : learner,
+            "materials" : material,
+            "certificates" : 0
+        }
         console.log(course);
-        res.status(200).send({"Message" : "success","data" : course})            
+        res.status(200).send({"Message" : "success","data" : data})            
     }
     catch (error) {
         console.log("In Catch..");
